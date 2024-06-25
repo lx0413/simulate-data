e4c797f66f2e6cd4c3997a849b80fefdc0469e33
Xiang LI <xiangli@lclark.edu>
2024-06-25 16:04:21 -0700

    initial commit

---
+++ script.R
@@ -0,0 +1,13 @@
+# Simulate and plot data 
+# Xiang LI
+# xiangli@lclark.edu
+# 2024-06-25
+
+# Simulate predictor variable 
+x <- rnorm(n = 100)
+
+# Simulate response variable with some noise
+y <- 2*x+rnorm(n = 100, sd = 0.2)
+
+# Plot the data
+plot(x = x, y = y)
